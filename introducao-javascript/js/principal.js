0e47a786ed47161b737f7fd68e9126abdbadcf9f
Arie_Azzi <ariel_azzi@hotmail.com>
2017-07-05 22:44:29 -0300

    Escutando eventos

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -6,36 +6,43 @@ var pacientes = document.querySelectorAll(".paciente");
 
 for (var i = 0; i < pacientes.length; i++) {
 
-  var paciente = pacientes[i];
+    var paciente = pacientes[i];
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-  var tdPeso = paciente.querySelector(".info-peso");
-  var peso = tdPeso.textContent;
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-  var tdAltura = paciente.querySelector(".info-altura");
-  var altura = tdAltura.textContent;
+    var tdImc = paciente.querySelector(".info-imc");
 
-  var tdImc = paciente.querySelector(".info-imc");
-
-  var pesoEhValido = true;
-  var alturaEhValido = true;
+    var pesoEhValido = true;
+    var alturaEhValido = true;
 
 
-  if(peso <= 0 || peso > 100) {
-    console.log("Peso Inválido!");
-    pesoEhValido = false;
-    tdImc.textContent = "Peso Inválido!";
-    paciente.classList.add("paciente-invalido");
-  }
+    if(peso <= 0 || peso > 100) {
+        console.log("Peso Inválido!");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso Inválido!";
+        paciente.classList.add("paciente-invalido");
+    }
 
-  if(altura <= 0 || altura >= 3.00) {
-    console.log("Altura Inválida!");
-    alturaEhValido = false;
-    tdImc.textContent = "Altura Inválido!";
-    paciente.classList.add("paciente-invalido");
-  }
+    if(altura <= 0 || altura >= 3.00) {
+        console.log("Altura Inválida!");
+        alturaEhValido = false;
+        tdImc.textContent = "Altura Inválido!";
+        paciente.classList.add("paciente-invalido");
+    }
 
-  if(pesoEhValido && alturaEhValido) {
-    var imc = peso / (altura  * altura);
-    tdImc.textContent = imc.toFixed(2);
-  }
+    if(pesoEhValido && alturaEhValido) {
+        var imc = peso / (altura  * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+
+
+}
+
+titulo.addEventListener('click', mostraMensagem);
+
+function mostraMensagem() {
+    console.log("olá eu fui clicado!")    
 }
