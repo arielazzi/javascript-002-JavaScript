f87248e539e69640d2f0807c7f5a1f69cfae0fe2
arielazzi <ariel_azzi@hotmail.com>
2017-08-01 14:00:14 -0300

    Quebrando o código em arquivos diferentes

---
+++ introducao-javascript/js/calcula-imc.js
@@ -0,0 +1,45 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++) {
+
+    var paciente = pacientes[i];
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoEhValido = true;
+    var alturaEhValido = true;
+
+
+    if(peso <= 0 || peso > 100) {
+        console.log("Peso Inválido!");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso Inválido!";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(altura <= 0 || altura >= 3.00) {
+        console.log("Altura Inválida!");
+        alturaEhValido = false;
+        tdImc.textContent = "Altura Inválido!";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(pesoEhValido && alturaEhValido) {
+        var imc = peso / (altura  * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+
+
+}
+
+
+
